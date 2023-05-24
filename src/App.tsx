62989aba7c8fe51f4d563a032777a622201cbd9c
Evgeniy <golowin056@gmail.com>
2023-05-24 17:30:06 +0300

    add husky

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,9 @@ import React from "react";
 import {RouterProvider} from "react-router-dom";
 import {router} from "./view/router";
 import {ThemeProvider} from "@mui/material";
-import {theme} from "./init/constants";
 import {Provider} from "react-redux";
 import store from "./init/store/store";
+import {theme} from "./themes/theme";
 
 export const App = () => {
     return <Provider store={store}><ThemeProvider theme={theme}> <RouterProvider
