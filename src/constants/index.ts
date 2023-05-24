d5ebf55b54895495e07d14583583e039316d2ee1
Evgeniy <golowin056@gmail.com>
2023-05-24 14:09:38 +0300

    add constants

---
+++ src/constants/index.ts
@@ -0,0 +1,4 @@
+export const YELLOW = 'rgba(230, 126, 34, 1)';
+export const GREY = 'rgba(233, 233, 233, 1)';
+export const BLUE = 'rgba(52, 152, 219, 1)';
+export const GREEN = 'rgba(46, 204, 113, 1)';
